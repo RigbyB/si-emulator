66ad1b20a8bcdc5e5bf8216fa9e79c379e9fcc88
RigbyB <terryyyd@gmail.com>
2023-04-25 23:42:26 +0100

    Refactored decoder

--- cpu.cpp
+++ cpu.cpp
@@ -1,5 +1,4 @@
 #include "cpu.h"
-#include <iostream>
 #include "decoder.h"
 
 void CPU::tick() {
--- decoder.cpp
+++ decoder.cpp
@@ -1,273 +1,247 @@
 #include "decoder.h"
 #include <unordered_map>
-#include <iostream>
 #include "cpu.h"
 
-
-void nop_handler(CPU &cpu) {
-    std::cout << "NOP\n";
+// Generic functions
+void mvi(CPU &cpu, uint8_t &reg) {
+    reg = cpu.get_next();
 }
 
-void lxi_b_handler(CPU &cpu) {
-    const auto low = cpu.get_next();
-    const auto high = cpu.get_next();
-    cpu.b = high;
-    cpu.c = low;
-    const auto val = (high << 8) | low;
-    std::cout << "LXI B, " << std::hex << static_cast<int>(val) << "\n";
+void lxi(CPU &cpu, uint8_t &reg_a, uint8_t &reg_b) {
+    // Little endian
+    reg_b = cpu.get_next();
+    reg_a = cpu.get_next();
 }
 
-void dcr_b_handler(CPU &cpu) {
-    cpu.b--;
-
-    cpu.set_zero(cpu.b == 0);
-    // TODO: Set rest of flags
-
-    std::cout << "DCR B\n";
+void lxi(CPU &cpu, uint16_t &reg) {
+    // Little endian
+    reg = cpu.get_next_word();
 }
 
-void mvi_b_handler(CPU &cpu) {
-    const auto val = cpu.get_next();
-    cpu.b = val;
-    std::cout << "MVI B, " << std::hex << static_cast<int>(val) << "\n";
+void inx(CPU &cpu, uint8_t &reg_a, uint8_t &reg_b) {
+    reg_b++;
+
+    if (reg_b == 0)
+        reg_a++;
 }
 
-void dad_b_handler(CPU &cpu) {
-    const auto hl = (cpu.h << 8) | cpu.l;
-    const auto bc = (cpu.b << 8) | cpu.c;
-    const auto result = hl + bc;
+void dad(CPU &cpu, const uint8_t reg_a, const uint8_t reg_b) {
+    const uint16_t hl = cpu.h << 8 | cpu.l;
+    const uint16_t ab = reg_a << 8 | reg_b;
+
+    const uint16_t result = hl + ab;
+
     cpu.h = (result >> 8) & 0xFF;
     cpu.l = result & 0xFF;
 
-    // TODO: Set flags
-
-    std::cout << "DAD B\n";
+    // TODO: Set flag
 }
 
-void ldax_b_handler(CPU &cpu) {
-    const auto addr = (cpu.b << 8) | cpu.c;
-    cpu.a = cpu.memory.read(addr);
-    std::cout << "LDAX B\n";
+void mov_m(CPU &cpu, const uint8_t &reg) {
+    cpu.memory.write(cpu.h << 8 | cpu.l, reg);
 }
 
-void mvi_c_handler(CPU &cpu) {
-    const auto val = cpu.get_next();
-    cpu.c = val;
-    std::cout << "MVI C, " << std::hex << static_cast<int>(val) << "\n";
+void ldax(CPU &cpu, const uint8_t reg_a, const uint8_t reg_b) {
+    cpu.a = cpu.memory.read(reg_a << 8 | reg_b);
 }
 
-void lxi_d_handler(CPU &cpu) {
-    const auto low = cpu.get_next();
-    const auto high = cpu.get_next();
-    cpu.d = high;
-    cpu.e = low;
-    const auto val = (high << 8) | low;
-    std::cout << "LXI D, " << std::hex << static_cast<int>(val) << "\n";
+void push(CPU &cpu, const uint8_t reg_a, const uint8_t reg_b) {
+    cpu.sp -= 2;
+
+    cpu.memory.write_word(cpu.sp, reg_a << 8 | reg_b);
 }
 
-void inx_d_handler(CPU &cpu) {
-    cpu.e++;
+void pop(CPU &cpu, uint8_t &reg_a, uint8_t &reg_b) {
+    const uint16_t val = cpu.memory.read_word(cpu.sp);
 
-    if (cpu.e == 0)
-        cpu.d++;
+    cpu.sp += 2;
 
-    std::cout << "INX D\n";
+    reg_a = (val >> 8) & 0xFF;
+    reg_b = val & 0xFF;
 }
 
-void dad_d_handler(CPU &cpu) {
-    const auto hl = (cpu.h << 8) | cpu.l;
-    const auto de = (cpu.d << 8) | cpu.e;
-    const auto result = hl + de;
-    cpu.h = (result >> 8) & 0xFF;
-    cpu.l = result & 0xFF;
+void dcr(CPU &cpu, uint8_t &reg) {
+    reg--;
 
     // TODO: Set flags
-
-    std::cout << "DAD D\n";
 }
 
-void ldax_d_handler(CPU &cpu) {
-    const auto addr = (cpu.d << 8) | cpu.e;
-    cpu.a = cpu.memory.read(addr);
-    std::cout << "LDAX D\n";
-}
-
-
-void lxi_h_handler(CPU &cpu) {
-    const auto low = cpu.get_next();
-    const auto high = cpu.get_next();
-    cpu.h = high;
-    cpu.l = low;
-    const auto val = (high << 8) | low;
-    std::cout << "LXI H, " << std::hex << static_cast<int>(val) << "\n";
-}
-
-void inx_h_handler(CPU &cpu) {
-    cpu.l++;
-
-    if (cpu.l == 0)
-        cpu.h++;
-
-    std::cout << "INX H\n";
-}
-
-void mvi_h_handler(CPU &cpu) {
-    const auto val = cpu.get_next();
-    cpu.h = val;
-    std::cout << "MVI H, " << std::hex << static_cast<int>(val) << "\n";
-}
-
-void dad_h_handler(CPU &cpu) {
-    const auto hl = (cpu.h << 8) | cpu.l;
-    const auto result = hl + hl;
-    cpu.h = (result >> 8) & 0xFF;
-    cpu.l = result & 0xFF;
-
-    // TODO: Set flags
-
-    std::cout << "DAD H\n";
-}
-
-void lxi_sp_handler(CPU &cpu) {
-    const auto addr = cpu.get_next_word();
-    cpu.sp = addr;
-    std::cout << "LXI SP, " << std::hex << static_cast<int>(addr) << "\n";
-}
-
-void sta_handler(CPU &cpu) {
-    const auto addr = cpu.get_next_word();
-    cpu.memory.write(addr, cpu.a);
-    std::cout << "STA " << std::hex << static_cast<int>(addr) << "\n";
-}
-
-void lda_handler(CPU &cpu) {
-    const auto addr = cpu.get_next_word();
-    cpu.a = cpu.memory.read(addr);
-    std::cout << "LDA " << std::hex << static_cast<int>(addr) << "\n";
-}
-
-void mvi_a_handler(CPU &cpu) {
-    const auto val = cpu.get_next();
-    cpu.a = val;
-    std::cout << "MVI A, " << std::hex << static_cast<int>(val) << "\n";
-}
-
-void mov_l_a_handler(CPU &cpu) {
-    cpu.l = cpu.a;
-    std::cout << "MOV L, A\n";
-}
-
-void mov_m_a_handler(CPU &cpu) {
-    const auto addr = (cpu.h << 8) | cpu.l;
-    cpu.memory.write(addr, cpu.a);
-    std::cout << "MOV M, A\n";
-}
-
-void ana_a_handler(CPU &cpu) {
-    cpu.a &= cpu.a;
+void ana(CPU &cpu, const uint8_t &reg) {
+    cpu.a &= reg;
 
     // TOOD: Set flags
-
-    std::cout << "ANA A\n";
 }
 
-void xra_a_handler(CPU &cpu) {
-    cpu.a ^= cpu.a;
+void xra(CPU &cpu, const uint8_t &reg) {
+    cpu.a ^= reg;
 
     // TODO: Set flags
-
-    std::cout << "XRA A\n";
 }
 
-void ora_c_handler(CPU &cpu) {
+void ora(CPU &cpu, uint8_t &reg) {
     cpu.a |= cpu.c;
 
     // TODO: Set flags
-
-    std::cout << "ORA C\n";
 }
 
-void pop_b_handler(CPU &cpu) {
-    const auto val = cpu.memory.read_word(cpu.sp);
-    cpu.sp += 2;
-    cpu.b = (val >> 8) & 0xFF;
-    cpu.c = val & 0xFF;
-    std::cout << "POP B\n";
+// Handlers
+void nop_handler(CPU &cpu) {}
+
+
+#define MVI_HANDLER(reg) void mvi_##reg##_handler(CPU &cpu) { mvi(cpu, cpu.reg); }
+
+MVI_HANDLER(a)
+
+MVI_HANDLER(b)
+
+MVI_HANDLER(c)
+
+MVI_HANDLER(d)
+
+MVI_HANDLER(e)
+
+MVI_HANDLER(h)
+
+MVI_HANDLER(l)
+
+
+#define LXI_HANDLER(reg_a, reg_b) void lxi_##reg_a##_handler(CPU &cpu) { lxi(cpu, cpu.reg_a, cpu.reg_b); }
+
+LXI_HANDLER(b, c)
+
+LXI_HANDLER(d, e)
+
+LXI_HANDLER(h, l)
+
+void lxi_sp_handler(CPU &cpu) {
+    lxi(cpu, cpu.sp);
+}
+
+
+void ldax_b_handler(CPU &cpu) {
+    ldax(cpu, cpu.b, cpu.c);
+}
+
+void ldax_d_handler(CPU &cpu) {
+    ldax(cpu, cpu.d, cpu.e);
+}
+
+
+void lda_handler(CPU &cpu) {
+    cpu.a = cpu.memory.read(cpu.get_next_word());
+}
+
+
+#define INX_HANDLER(reg_a, reg_b) void inx_##reg_a##_handler(CPU &cpu) { inx(cpu, cpu.reg_a, cpu.reg_b); }
+
+INX_HANDLER(d, e)
+
+INX_HANDLER(h, l)
+
+
+#define DAD_HANDLER(reg_a, reg_b) void dad_##reg_a##_handler(CPU &cpu) { dad(cpu, cpu.reg_a, cpu.reg_b); }
+
+DAD_HANDLER(b, c)
+
+DAD_HANDLER(d, e)
+
+DAD_HANDLER(h, l)
+
+
+#define MOV_HANDLER(reg_a, reg_b) void mov_##reg_a##_##reg_b##_handler(CPU &cpu) { cpu.reg_a = cpu.reg_b; }
+
+MOV_HANDLER(l, a)
+
+
+#define MOV_M_HANDLER(reg) void mov_m_##reg##_handler(CPU &cpu) { mov_m(cpu, cpu.reg); }
+
+MOV_M_HANDLER(a)
+
+
+#define PUSH_HANDLER(reg_a, reg_b) void push_##reg_a##_handler(CPU &cpu) { push(cpu, cpu.reg_a, cpu.reg_b); }
+
+PUSH_HANDLER(b, c)
+
+PUSH_HANDLER(d, e)
+
+PUSH_HANDLER(h, l)
+
+
+#define POP_HANDLER(reg_a, reg_b) void pop_##reg_a##_handler(CPU &cpu) { pop(cpu, cpu.reg_a, cpu.reg_b); }
+
+POP_HANDLER(b, c)
+
+POP_HANDLER(d, e)
+
+POP_HANDLER(h, l)
+
+
+#define DCR_HANDLER(reg) void dcr_##reg##_handler(CPU &cpu) { dcr(cpu, cpu.reg); }
+
+DCR_HANDLER(b)
+
+
+#define ANA_HANDLER(reg) void ana_##reg##_handler(CPU &cpu) { ana(cpu, cpu.reg); }
+
+ANA_HANDLER(a)
+
+
+#define XRA_HANDLER(reg) void xra_##reg##_handler(CPU &cpu) { xra(cpu, cpu.reg); }
+
+XRA_HANDLER(a)
+
+
+#define ORA_HANDLER(reg) void ora_##reg##_handler(CPU &cpu) { ora(cpu, cpu.reg); }
+
+ORA_HANDLER(c)
+
+void sta_handler(CPU &cpu) {
+    cpu.memory.write(cpu.get_next_word(), cpu.a);
+}
+
+
+void jmp_handler(CPU &cpu) {
+    cpu.pc = cpu.get_next_word();
 }
 
 void jnz_handler(CPU &cpu) {
-    const auto addr = cpu.get_next_word();
-
-    if (cpu.is_zero() == 0)
-        cpu.pc = addr;
-
-    std::cout << "JNZ " << std::hex << static_cast<int>(addr) << "\n";
+    if (!cpu.is_zero())
+        cpu.pc = cpu.get_next_word();
 }
 
-void jmp_handler(CPU &cpu) {
+void call_handler(CPU &cpu) {
     const auto addr = cpu.get_next_word();
-    cpu.pc = addr;
-    std::cout << "JMP " << std::hex << static_cast<int>(addr) << "\n";
-}
 
-void push_b_handler(CPU &cpu) {
     cpu.sp -= 2;
-    cpu.memory.write_word(cpu.sp, (cpu.b << 8) | cpu.c);
-    std::cout << "PUSH B\n";
+
+    // The program counter is now after the arguments because of get_next_word
+    cpu.memory.write_word(cpu.sp, cpu.pc);
+
+    cpu.pc = addr;
 }
 
 void ret_handler(CPU &cpu) {
     cpu.pc = cpu.memory.read_word(cpu.sp);
     cpu.sp += 2;
-    std::cout << "RET\n";
-}
-
-void call_handler(CPU &cpu) {
-    const auto addr = cpu.get_next_word();
-    cpu.sp -= 2;
-    // This is the location after the call instruction and arguments
-    cpu.memory.write_word(cpu.sp, cpu.pc + 2);
-    cpu.pc = addr;
-    std::cout << "CALL " << std::hex << static_cast<int>(addr) << "\n";
 }
 
 void out_handler(CPU &cpu) {
     const auto port = cpu.get_next();
 
     // TODO: Implement output
-
-    std::cout << "OUT " << std::hex << static_cast<int>(port) << "\n";
-}
-
-void push_d_handler(CPU &cpu) {
-    cpu.sp -= 2;
-    cpu.memory.write_word(cpu.sp, (cpu.d << 8) | cpu.e);
-    std::cout << "PUSH D\n";
-}
-
-void pop_h_handler(CPU &cpu) {
-    const auto val = cpu.memory.read_word(cpu.sp);
-    cpu.sp += 2;
-    cpu.h = (val >> 8) & 0xFF;
-    cpu.l = val & 0xFF;
-    std::cout << "POP H\n";
-}
-
-void push_h_handler(CPU &cpu) {
-    cpu.sp -= 2;
-    cpu.memory.write_word(cpu.sp, (cpu.h << 8) | cpu.l);
-    std::cout << "PUSH H\n";
 }
 
 void xchg_handler(CPU &cpu) {
     const auto hl = (cpu.h << 8) | cpu.l;
     const auto de = (cpu.d << 8) | cpu.e;
+
     cpu.h = (de >> 8) & 0xFF;
     cpu.l = de & 0xFF;
+
     cpu.d = (hl >> 8) & 0xFF;
     cpu.e = hl & 0xFF;
-    std::cout << "XCHG\n";
 }
+
 
 const std::unordered_map<uint8_t, InstructionHandler> instructions = {
         {0x00, nop_handler},
